e1bb4dcd5cae99dc561fac93327a494ed1d6ea7e
NicatQurbanov <nicatqur1307@gmail.com>
2025-10-29 21:04:23 +0400

    to do tomorrow

---
+++ script.js
@@ -0,0 +1,16 @@
+const gunArr = [
+    {
+      model: 'AK-74',
+      type: '',
+      caliber: '',
+      weight: '',
+      magazine: '',
+      price: '',
+      modifications: '',
+      src: ''
+    },
+     
+]
+
+
+const gunList = document.getElementById('gun-list')
