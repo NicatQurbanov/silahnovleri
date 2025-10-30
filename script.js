94acecd11a953523bfbdd57f6f5f17f6f86345c7
NicatQurbanov <nicatqur1307@gmail.com>
2025-10-30 21:22:29 +0400

    to be continued 30/10/2025

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { gunArr } from './Data/data.js'
 const gunList = document.getElementById('gun-list')
 const gunImg = document.getElementById('img-container')
 const descBar = document.getElementById('description-bar')
+const charasteristics = document.getElementById('charasteristics')
 let arrList = '';
 gunArr.forEach(gun => {
   arrList += `
@@ -15,8 +16,19 @@ gunArr.forEach(gun => {
 document.addEventListener('click', function(e){
   gunArr.forEach(gun => {
     if (e.target.id === gun.model)  {
+      
       gunImg.innerHTML = `<img class="gun-image" src="${gun.src}">`
       descBar.innerHTML  = `<p class="description-paragraph">The ${gun.model} ${gun.text}</p>`
+      charasteristics.innerHTML =
+      `<h2>Product description</h2>
+          <li><p>Model:</p><span class="charasteristics-data">${gun.model}</span></li>
+          <li><p>Type:</p><span class="charasteristics-data">${gun.type}</span></li>
+          <li><p>Caliber:</p><span class="charasteristics-data">${gun.caliber}mm</span></li>
+          <li><p>Weight:</p><span class="charasteristics-data">${gun.weight}kg</span></li>
+          <li><p>Magazine:</p><span class="charasteristics-data">${gun.magazine}</span></li>
+          <li><p>Max range:</p><span class="charasteristics-data">${gun.rangeMax}m</span></li>
+          <li><p>Modifications:</p><span class="charasteristics-data">${gun.modifications}</span></li>
+          <li><p>Price:</p><span class="charasteristics-data">${gun.price}$</span></p></li>`
       }
   })
 })
