0672fc2ef396cf1d75d0ab5767a66cfabe0cfdad
NicatQurbanov <nicatqur1307@gmail.com>
2025-11-02 22:00:50 +0400

    to be continued

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const gunList = document.getElementById('gun-list')
 const gunImg = document.getElementById('img-container')
 const descBar = document.getElementById('description-bar')
 const charasteristics = document.getElementById('charasteristics')
-const buyBtn = document.getElementById('buy-btn')
 const balance = document.getElementById('balance-num')
+const buyBtn = document.getElementById('buy-btn')
 
 let arrList = '';
 gunArr.forEach(gun => {
@@ -16,43 +16,38 @@ gunArr.forEach(gun => {
     </li>
     `;
 })
+
 gunList.innerHTML = arrList;
 
 document.addEventListener('click', e => {
   gunArr.forEach(gun => {
+    let keyArr = Object.keys(gun)
     if (e.target.id === gun.model)  {
       buyBtn.classList.remove('inactive')
       gunImg.innerHTML = `<img class="gun-image" src="${gun.src}">`
       descBar.innerHTML  = `<p class="description-paragraph">The ${gun.model} ${gun.text}</p>`
-      charasteristics.innerHTML =
-      `<h2>Product description</h2>
-          <li><p>Model:</p><span class="charasteristics-data">${gun.model}</span></li>
-          <li><p>Type:</p><span class="charasteristics-data">${gun.type}</span></li>
-          <li><p>Caliber:</p><span class="charasteristics-data">${gun.caliber}mm</span></li>
-          <li><p>Weight:</p><span class="charasteristics-data">${gun.weight}kg</span></li>
-          <li><p>Magazine:</p><span class="charasteristics-data">${gun.magazine}</span></li>
-          <li><p>Max range:</p><span class="charasteristics-data">${gun.rangeMax}m</span></li>
-          <li><p>Modifications:</p><span class="charasteristics-data">${gun.modifications}</span></li>
-          <li><p>Price:</p><span class="charasteristics-data">${gun.price}$</span></p></li>`
+      let charasteristicsText = '';
+      keyArr.forEach( key => {
+        if (key === "text"|| key === "src"|| key === "hasModifications") {
+             return
+        }
+        charasteristicsText += `<li class="list"><p>${key}:</p><span class="charasteristics-data">${gun[key]}</span></p></li>`
+      })
+        charasteristics.innerHTML = `<h2>Product description</h2> ${charasteristicsText}`;
       }
-      if (e.target.id === buyBtn.id) {
+    })
+    // Secilmis mehsulun "Buy" button uzerine basanda hemis mehsulun qiymeti user-in balansindan cixmalidi
+    if (e.target.id === buyBtn.id) {
     let balanceNum = Number(balance.innerHTML)
-    let priceNum =  Number(gun.price)
+    console.log(e)
+    let priceNum =  Number(gunArr[0].price) 
+    // gunArr[0] evezine current secilmis mehsulu Number() icine nece yaza bilerem?????
     if (balanceNum > priceNum) {
       const hh = balanceNum - priceNum
       balance.innerHTML = hh.toString()
-      console.log(balance)
-    }
-  }
-    })
-  
+      console.log(balance)}}
 })
-console.log(Number(balance.innerHTML) - Number(gunArr[0].price))
 
-for (let i = 0; i < gunArr.length; i++) {
-  let text1 = ''
-  text1 += `<li><p>${gunArr[i].}</p><span class="charasteristics-data">${gunArr[i].}</span></li>`
-}
 
 
 
